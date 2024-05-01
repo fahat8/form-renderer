2305a2d74e1eeefd57b5098d16a0debdda74c82e
Fabian Härtel <fhaertel@transform8.de>
2024-05-01 19:00:10 +0200

    Split pane in two

--- src/components/SurveyCreator.vue
+++ src/components/SurveyCreator.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="container">
-        <!-- Droppable Section 1 -->
-        <div class="drop-zone" @drop="onDrop($event, 1)" @dragover.prevent="() => { }" @dragenter.prevent="() => { }">
+    <div class="container" style="display: flex;">
+        <!-- Source Column (Left) -->
+        <div class="drop-zone" style="width: 50%; float: left;">
             <div v-for="item in getList(1)" :key="item.id" class="drag-el" draggable="true"
-                @dragstart="startDrag($event, item)">
+                @dragstart="startDrag($event, item, 1)">
                 {{ item.title }}
             </div>
         </div>
-        <!-- Droppable Section 2 -->
-        <div class="drop-zone" @drop="onDrop($event, 2)" @dragover.prevent="() => { }" @dragenter.prevent="() => { }">
-            <div v-for="item in getList(2)" :key="item.id" class="drag-el" draggable="true"
-                @dragstart="startDrag($event, item)">
+        <!-- Target Column (Right) -->
+        <div class="drop-zone" @drop="onDrop($event)" @dragover.prevent="() => { }" @dragenter.prevent="() => { }"
+            style="width: 50%; float: right;">
+            <div v-for="item in getList(2)" :key="`target-${item.id}`" class="drag-el" draggable="true"
+                @dragstart="startDrag($event, item, 2)" @dragover="dragOver($event, index)">
                 {{ item.title }}
             </div>
         </div>
     </div>
 </template>
+
 
 <script>
 import { ref } from 'vue'
@@ -25,28 +27,44 @@ export default {
         const items = ref([
             { id: 0, title: 'Item A', list: 1 },
             { id: 1, title: 'Item B', list: 1 },
-            { id: 2, title: 'Item C', list: 2 },
-        ])
+            { id: 3, title: 'Item C', list: 1 },
+            { id: 4, title: 'Item D', list: 1 },
+            { id: 5, title: 'Item E', list: 1 }
+        ]);
+
+        const nextId = ref(6);
+        const dropIndex = ref(-1);
 
         const getList = (list) => {
-            return items.value.filter((item) => item.list == list)
+            return items.value.filter(item => item.list === list);
         }
 
-        const startDrag = (evt, item) => {
-            evt.dataTransfer.dropEffect = 'move'
-            evt.dataTransfer.effectAllowed = 'move'
-            evt.dataTransfer.setData('itemID', item.id.toString())
+        const startDrag = (evt, item, origin) => {
+            evt.dataTransfer.dropEffect = 'move';
+            evt.dataTransfer.effectAllowed = 'all';
+            evt.dataTransfer.setData('itemID', item.id.toString());
+            evt.dataTransfer.setData('origin', origin.toString());
         }
 
-        const onDrop = (evt, list) => {
-            const itemID = parseInt(evt.dataTransfer.getData('itemID'))
-            const item = items.value.findIndex((item) => item.id === itemID)
-            if (item !== -1) {
-                // Ensure we're modifying the object, not overwriting an array index
-                items.value[item].list = list;
-                console.log('Updated item:', items.value[item]);
-            } else {
-                console.log('Item not found with ID:', itemID);
+        const dragOver = (evt, index) => {
+            dropIndex.value = index;
+            evt.preventDefault();
+            console.log(dropIndex.value);
+        }
+
+        const onDrop = (evt) => {
+            const itemID = parseInt(evt.dataTransfer.getData('itemID'));
+            const origin = parseInt(evt.dataTransfer.getData('origin'));
+            const droppedItem = items.value.find(item => item.id === itemID);
+
+            if (droppedItem) {
+                if (origin === 1 && droppedItem.list === 1) { // From left to right
+                    const newItem = { ...droppedItem, id: nextId.value++, list: 2 };
+                    items.value.push(newItem);
+                } else if (origin === 2) { // Reorder elements in right list
+                    items.value.splice(items.value.findIndex(i => i.id === itemID), 1); // Remove the item from its current position
+                    items.value.splice(dropIndex.value, 0, droppedItem);
+                }
             }
         }
 
@@ -54,24 +72,34 @@ export default {
             getList,
             onDrop,
             startDrag,
+            dragOver,
         }
     },
 }
 </script>
 
+
+
 <style>
+.container {
+    display: flex;
+    justify-content: space-between;
+}
+
 .drop-zone {
     width: 50%;
-    margin: 50px auto;
+    margin: 0;
     background-color: #eee;
-    margin-bottom: 10px;
     padding: 10px;
-    min-height: 10px;
+    min-height: 100px;
+    /* Ensure there's space to drop */
 }
 
 .drag-el {
     background-color: #fff;
     margin-bottom: 10px;
     padding: 5px;
+    cursor: pointer;
+    /* Indicates it's draggable */
 }
 </style>
